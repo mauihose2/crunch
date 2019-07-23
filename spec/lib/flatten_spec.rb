91057c74561f6109f25baf74b20870d725396269
Hathawanian <mauihose2@gmail.com>
2019-07-23 20:08:18 +1200

    add another expectation
    * add another expectation

--- spec/lib/flatten_spec.rb
+++ spec/lib/flatten_spec.rb
@@ -4,15 +4,17 @@ require 'spec_helper'
 
 RSpec.describe Crunch::Array.flatten do
   # TODO: add more comprehensive specs
-  it 'it flattens array' do
+  it 'it flattens arrays' do
     a1 = [1, [2, 3], [4], 5]
     a2 = [1, 2, 3, [4, 5]]
     a3 = [[1], [2], [3], [4], [5]]
+    a4 = [[1, 2], 3, [4, 5]]
 
     ans = [1, 2, 3, 4, 5]
 
     expect(Crunch::Array.flatten(a1)).to eq ans
     expect(Crunch::Array.flatten(a2)).to eq ans
     expect(Crunch::Array.flatten(a3)).to eq ans
+    expect(Crunch::Array.flatten(a4)).to eq ans
   end
 end
